f472baaf912d3aca48955c856ac85e29be732fde
Chelsea Kent <chelsea.kent@gmail.com>
2018-08-27 13:45:43 -0700

    finish tic tac toe functionality

--- app.js
+++ app.js
@@ -8,59 +8,126 @@
 // I can click new game at any time to reset the board
 
 const state = {
-  cells: [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+  cells: [null, null, null, null, null, null, null, null, null],
   clear: function() {
-    this.cells = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '];
-    console.log('it works');
+    this.cells = [null, null, null, null, null, null, null, null, null];
   },
-  currentMarker: 'X'
+  currentMarker: 'X',
+  endOfGame: false
 };
 
 // State modification functions
-function clearState(){
+
+//clear state back to empty
+function clearState() {
   state.clear();
 }
 
-function switchCurrentMarker(){
-  switch(state.currentMarker){
-  case 'X':
-    state.currentMarker = 'O';
-    break;
-  case 'O':
-    state.currentMarker = 'X';
-    break;  
+// switch current marker O to X etc.
+function switchCurrentMarker() {
+  switch (state.currentMarker) {
+    case 'X':
+      state.currentMarker = 'O';
+      break;
+    case 'O':
+      state.currentMarker = 'X';
+      break;
   }
 }
 
-function markCell(index){
-  state.cells[index] = state.currentMarker;
-  switchCurrentMarker();
+// mark cell with current marker
+function markCell(index) {
+  if (state.cells[index] === null) {
+    state.cells[index] = state.currentMarker;
+    switchCurrentMarker();
+  }
 }
+
+//determine winner
+function determineWinner() {
+  const winnerPatterns = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [2, 4, 6],
+    [0, 4, 8]
+  ];
+
+  for (let i = 0; i < winnerPatterns.length; i++) {
+    if (
+      state.cells[winnerPatterns[i][0]] === state.cells[winnerPatterns[i][1]] &&
+      state.cells[winnerPatterns[i][1]] === state.cells[winnerPatterns[i][2]] &&
+      state.cells[winnerPatterns[i][0]] !== null
+    ) {
+      $(`#${winnerPatterns[i][0]}`).addClass('win');
+      $(`#${winnerPatterns[i][1]}`).addClass('win');
+      $(`#${winnerPatterns[i][2]}`).addClass('win');
+      // disable click on cells after we have a winner
+      state.endOfGame = true;
+    }
+  }
+}
+
 // Render functions
 
 function render() {
   let htmlArr = [];
-  for (let i=0; i<state.cells.length; i++){
-    let html = `
-    <div class="cell" id="${i}">
-    <p>${state.cells[i]}</p>
-    </div>
-    `;
+  // for loop through state cells length and generate html
+  for (let i = 0; i < state.cells.length; i++) {
+    let html = '';
+    if (i % 3 === 0) {
+      html = `
+      <div class="row">
+      <div class="cell" id="${i}">
+      <p>${state.cells[i] ? state.cells[i] : '&nbsp;'}</p>
+      </div>
+      `;
+    } else if (i % 3 === 2) {
+      html = `
+      <div class="cell" id="${i}">
+      <p>${state.cells[i] ? state.cells[i] : '&nbsp;'}</p>
+      </div>
+      </div>
+      `;
+    } else {
+      html = `
+      <div class="cell" id="${i}">
+      <p>${state.cells[i] ? state.cells[i] : '&nbsp;'}</p>
+      </div>
+      `;
+    }
+
     htmlArr.push(html);
-  } $('.board').html(htmlArr.join(''));
+  }
+  // push html to DOM .board
+  $('.board').html(htmlArr.join(''));
+  // run determine winner function to see if anyone is winning
+  determineWinner();
+
+  //
+  if (state.endOfGame) {
+    $('.board').off('click', '.cell');
+  } else {
+    handleCellClick();
+  }
 }
 
 // Event Listeners
 
 function handleStartNewGame() {
-  $('#new-game').click(() => clearState());
-  render();
+  $('#new-game').click(() => {
+    clearState();
+    state.endOfGame = false;
+    render();
+  });
 }
 
 function handleCellClick() {
-  $('.board').on('click', '.cell', function(event){
+  $('.board').on('click', '.cell', function(event) {
     markCell(this.id);
-    console.log(this.id);
     render();
   });
 }
