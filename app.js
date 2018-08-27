dcf0cbbb6cb1ad8e53293dddf00ee067fefe4906
Cameron Hatch <cameronlavigne92@gmail.com>
2018-08-27 13:48:35 -0500

    built event listener struction in app.js

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+// Can immediately click cells to alternate between Xs and Os on each click
+// Cannot change cell if it has a value inside of it
+// I can see when a winning line has been created
+//  - no other moves can be put on the board
+// I can click new game at any time to reset the board
+
 const state = {
 };
 
