4bb69d37948237587169d2d74d7529f2e19ea06a
Cameron Hatch <cameronlavigne92@gmail.com>
2018-08-27 14:39:58 -0500

    render functionality

--- app.js
+++ app.js
@@ -8,27 +8,65 @@
 // I can click new game at any time to reset the board
 
 const state = {
-  cells: ['', '', '', '', '', '', '', '', ''],
+  cells: [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
   clear: function() {
-    this.cells = ['', '', '', '', '', '', '', '', ''];
-  }
+    this.cells = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '];
+    console.log('it works');
+  },
+  currentMarker: 'X'
 };
 
 // State modification functions
+function clearState(){
+  state.clear();
+}
 
+function switchCurrentMarker(){
+  switch(state.currentMarker){
+  case 'X':
+    state.currentMarker = 'O';
+    break;
+  case 'O':
+    state.currentMarker = 'X';
+    break;  
+  }
+}
+
+function markCell(index){
+  state.cells[index] = state.currentMarker;
+  switchCurrentMarker();
+}
 // Render functions
 
-function render() {}
+function render() {
+  let htmlArr = [];
+  for (let i=0; i<state.cells.length; i++){
+    let html = `
+    <div class="cell" id="${i}">
+    <p>${state.cells[i]}</p>
+    </div>
+    `;
+    htmlArr.push(html);
+  } $('.board').html(htmlArr.join(''));
+}
 
 // Event Listeners
 
 function handleStartNewGame() {
-  $('#new-game').click(() => state.clear());
+  $('#new-game').click(() => clearState());
+  render();
 }
 
-function handleCellClick() {}
+function handleCellClick() {
+  $('.board').on('click', '.cell', function(event){
+    markCell(this.id);
+    console.log(this.id);
+    render();
+  });
+}
 
 function main() {
+  render();
   handleCellClick();
   handleStartNewGame();
 }
